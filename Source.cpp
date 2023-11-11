9736358b01bf3bf9e0a6d6a67325b9f2354eb6b6
FunB4nan <127935709+FunB4nan@users.noreply.github.com>
2023-11-11 10:50:48 +0300

    LDL факторизация

--- Source.cpp
+++ Source.cpp
@@ -1,72 +1,71 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
+#include <vector>
 using namespace std;
-const int MAX_SIZE = 100;
-void gaussElimination(vector<vector<double>> A, vector<double> B) {
-    for (int i = 0; i < 3; i++) {
-        int maxRow = i;
-        for (int k = i + 1; k < 3; k++) {
-            if (abs(A[k][i]) > abs(A[maxRow][i])) {
-                maxRow = k;
-            }
-        }
-        for (int k = i; k < 3; k++) {
-            swap(A[maxRow][k], A[i][k]);
-        }
-        swap(B[maxRow], B[i]);
 
-        for (int k = i + 1; k < 3; k++) {
-            double factor = A[k][i] / A[i][i];
-            for (int j = i; j < 3; j++) {
-                A[k][j] -= factor * A[i][j];
+const int MAX_SIZE = 100;
+
+void LDLFactorization(vector<vector<double>> A, vector<double> B) {
+    double L[MAX_SIZE][MAX_SIZE];
+    double D[MAX_SIZE][MAX_SIZE];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j <= i; j++) {
+            double sum = 0;
+            if (j == i) {
+                for (int k = 0; k < j; k++) {
+                    sum += pow(L[j][k], 2) * D[k][k];
+                }
+                D[j][j] = A[j][j] - sum;
+                L[j][j] = 1;
             }
-            B[k] -= factor * B[i];
+            else {
+                for (int k = 0; k < j; k++) {
+                    sum += L[i][k] * D[k][k] * L[j][k];
+                }
+                L[i][j] = (A[i][j] - sum) / D[j][j];
+            }
         }
+    }
+
+    double y[MAX_SIZE];
+    for (int i = 0; i < 3; i++) {
+        double sum = 0;
+        for (int j = 0; j < i; j++) {
+            sum += L[i][j] * y[j];
+        }
+        y[i] = (B[i] - sum) / L[i][i];
+    }
+
+    double z[MAX_SIZE];
+    for (int i = 0; i < 3; i++) {
+        z[i] = y[i] / sqrt(D[i][i]);
     }
 
     double x[MAX_SIZE];
     for (int i = 2; i >= 0; i--) {
-        x[i] = B[i];
+        double sum = 0;
         for (int j = i + 1; j < 3; j++) {
-            x[i] -= A[i][j] * x[j];
+            sum += L[j][i] * x[j];
         }
-        x[i] /= A[i][i];
+        x[i] = (z[i] - sum) / L[i][i];
     }
-
-    double residual[MAX_SIZE];
-    double maxResidual = 0.0;
-    for (int i = 0; i < 3; i++) {
-        residual[i] = B[i];
-        for (int j = 0; j < 3; j++) {
-            residual[i] -= A[i][j] * x[j];
-        }
-        if (abs(residual[i]) > maxResidual) {
-            maxResidual = abs(residual[i]);
-        }
-    }
-
-    double relativeError = maxResidual / abs(x[0]);
 
     cout << "Solution vector:" << endl;
     for (int i = 0; i < 3; i++) {
         cout << "x[" << i << "] = " << x[i] << endl;
     }
-
-    cout << "Residual vector:" << endl;
-    for (int i = 0; i < 3; i++) {
-        cout << "residual[" << i << "] = " << residual[i] << endl;
-    }
-
-    cout << "Relative error: " << relativeError << endl;
 }
 
 int main() {
-    vector<vector<double>> A = { {2.31, 31.49, 1.52},
-                                {4.21, 22.42, 3.85},
-                                {3.49, 4.85, 28.72} };
+    double l1 = 1;
+    double l2 = pow(10, 3);
+    double l3 = pow(10, 6);
+    vector<vector<double>> A = { {2 * l1 + 4 * l2, 2 * (l1 - l2), 2 * (l1 - l2)},
+                            {2 * (l1 - l2), 2 * l1 + l2 + 3 * l3, 2 * l1 + l2 - 3 * l3},
+                            {2 * (l1 - l2), 2 * l1 + l2 - 3 * l3, 2 * l1 + l2 + 3 * l3} };
 
-    vector<double> b = { 40.95, 30.24, 42.81 };
+    vector<double> b = { -4 * l1 - 2 * l2, -4 * l1 + l2 + 9 * l3, -4 * l1 + l2 - 9 * l3 };
 
-    gaussElimination(A, b);
+    LDLFactorization(A, b);
 }
